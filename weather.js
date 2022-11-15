eb5dce95e2808134338d36c1f4abdabe8ea02f94
dkrishna11 <116519809+dkrishna11@users.noreply.github.com>
2022-11-15 16:21:44 +0530

    Add files via upload

---
+++ weather.js
@@ -0,0 +1,42 @@
+let result=document.getElementById("result");
+let searchBtn=document.getElementById("search-btn");
+let cityRef=document.getElementById("city");
+key="cca6c91283b3560da5d647b3fc5568e9";
+
+// function to fetch weather details    
+
+let getWeather=()=> {
+    let cityValue=cityRef.value;
+
+    // if input is empty
+
+    if (cityValue.length == 0) {
+        result.innerHTML = `<h3 class="msg">Please enter a city name</h3>`;
+      }
+
+    // if input is not empty
+
+    else{
+        let url=`https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${key}&units=metric`;
+        cityRef.value="";
+        fetch(url).then((resp)=> resp.json()).then((data) =>{
+            console.log(data);
+            result.innerHTML=`<h2>${data.name}</h2>
+            <h4 class="weather">${data.weather[0].main}</h4>
+            <h4 class="description">${data.weather[0].description}</h4>
+            <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}.png">
+            <h1>${data.main.temp} &#176;</h1>
+            <div class=temp-container>
+                <div>
+                    <h4 class="title">min</h4>
+                    <h4 class="temp">${data.main.temp_min} &#176;</h4>
+                </div>
+                <div>
+                    <h4 class="title">max</h4>
+                    <h4 class="temp">${data.main.temp_max} &#176;</h4>
+                </div>
+            </div>`;
+        }).catch(()=>{result.innerHTML=`<h3 class="msg"> City not Found</h3>`});
+    }    
+};
+searchBtn.addEventListener("click", getWeather);
